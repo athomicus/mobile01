b662c9fe87444d2436db1a0f54422e0c3eab77aa
athomicus <athomicus@gmail.com>
2024-04-12 22:03:26 +0200

    platforms

---
+++ platform.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+@onready var area_platform:Area3D = $"."
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	area_platform.body_entered.connect(_on_body_entered)
+
+ 
+func _on_body_entered(body):
+	
+	if body.is_in_group("PlayerGroup"):
+		body.jump()
+		print("Player Gruop")
+	'''
+	if body is Player:
+		print("Player from class_name")
+		print(body.velocity.y)
+		#if body.velocity.y < 0: # it means falling
+   '''
+		
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,23 +1,34 @@
 extends CharacterBody3D
+class_name Player
 @onready var camera = $"../Camera3D"
+var jump_velocity = 25.0
 
 const SPEED = 10.0
 var viewport_size
-
+var gravity = 1.0
+var max_fall_velocity = -10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready():
 	viewport_size = get_viewport().size
-	#print(viewport_size)
+	
 func _process(delta):
 	if Input.is_action_just_pressed("esc"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
 		
-
+	if Input.is_key_pressed(KEY_SPACE):
+		jump()
+		
 func _physics_process(delta):
+	
+	velocity.y -= gravity 
+	if velocity.y < max_fall_velocity:
+		velocity.y = max_fall_velocity #przelatuje
+		
+	print(velocity.y)
 	var direction = Input.get_axis("move_left","move_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -36,5 +47,8 @@ func _physics_process(delta):
 	#	global_position.x = -player_pos_from_viewport.x
 	#if global_position.x < -player_pos_from_viewport.x:
 	#	global_position.x = player_pos_from_viewport.x
-	
+func jump():
+	velocity.y = jump_velocity
 
+
+	
